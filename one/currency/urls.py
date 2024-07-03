90fa2b1e0a85ca0b9f8556e8ccc783a615142d78
your-correct-username <takhminaacom@gmail.com>
2024-07-03 16:53:06 +0600

    Committing changes before pull

--- one/currency/urls.py
+++ one/currency/urls.py
@@ -1,8 +1,9 @@
 # urls.py
 from django.urls import path
-from .views import CoinDataAPIView, FiatCurrencyListView
+from .views import CoinDataAPIView, FiatCurrencyListView, All
 
 urlpatterns = [
     path('api/coins/', CoinDataAPIView.as_view(), name='coin_data_api'),
     path('api/fiat-currencies/', FiatCurrencyListView.as_view(), name='fiat_currency_list_api'),
+    paht('api/all', All.as_view(), name='all_bank_cripta'),
 ]
