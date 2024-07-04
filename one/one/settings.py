c9b39fff1c89cc884d418f9b2d4f4008cffa923d
kamila <takhminaacom@gmail.com>
2024-07-04 16:04:50 +0600

    Resolved

--- one/one/settings.py
+++ one/one/settings.py
@@ -141,8 +141,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-# MEDIA_URL = 'http://127.0.0.1:8000//media/'
-MEDIA_URL = 'media/'
+MEDIA_URL = 'http://kamila1.pythonanywhere.com/media/'
+# MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
