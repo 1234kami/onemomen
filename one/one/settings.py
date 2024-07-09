24a1845c003ccf046a8bc389a0aa0d0949968472
kamila <takhminaacom@gmail.com>
2024-07-09 12:25:42 +0600

    Resolved

--- one/one/settings.py
+++ one/one/settings.py
@@ -138,23 +138,20 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
+# STATIC_ROOT = BASE_DIR / "staticfiles"
 
-<<<<<<< HEAD
+
 MEDIA_URL = 'http://kamila1.pythonanywhere.com/media/'
-=======
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-MEDIA_URL = 'https://kamila1.pythonanywhere.com/media/'
+# MEDIA_URL = 'https://kamila1.pythonanywhere.com/media/'
 
 
-
->>>>>>> origin/master
 # MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
